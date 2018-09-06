54aea1c913cbea88f707674f3913e5333684b25d
Doug-AWS <dougsch@amazon.com>
2018-09-05 17:02:25 -0700

    WIP

--- module-3/app/service/mysfitsTableClient.go
+++ module-3/app/service/mysfitsTableClient.go
@@ -1,73 +1,107 @@
 package main
 
 import (
-    "github.com/aws/aws-sdk-go/aws"
-    "github.com/aws/aws-sdk-go/aws/session"
-    "github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 
-    "fmt"
+	"io"
+	"log"
+	"os"
+)
+
+// Info is for logging
+var Info *log.Logger
+
+// Init initializes the logger
+func Init(
+	infoHandle io.Writer) {
+	Info = log.New(infoHandle,
+		"INFO: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
 }
 
-func getAllMysfits() {
-    // Create a DynamoDB client using our default credentials and region.
-    sess := session.Must(session.NewSessionWithOptions(session.Options{
-        SharedConfigState: session.SharedConfigEnable,
-    }))
-
-    // Create DynamoDB client
-    svc := dynamodb.New(sess)
-
-    // Retrieve all Mysfits from DynamoDB using the DynamoDB scan operation.
-    // Note: The scan API can be expensive in terms of latency when a DynamoDB
-    // table contains a high number of records and filters are applied to the
-    // operation that require a large amount of data to be scanned in the table
-    // before a response is returned by DynamoDB. For high-volume tables that
-    // receive many requests, it is common to store the result of frequent/common
-    // scan operations in an in-memory cache. DynamoDB Accelerator (DAX) or
-    // use of ElastiCache can provide these benefits. But, because out Mythical
-    // Mysfits API is low traffic and the table is very small, the scan operation
-    // will suit our needs for this workshop.
-    result := &dynamodb.ScanInput{
-        TableName: aws.String("MysfitsTable"),
-    }
-
-    // loop through the returned mysfits and add their attributes to a new map
-    // that matches the JSON response structure expected by the frontend.
-    for _, item := range result.items {
-
-    mysfit = make(map[string]string)
-
-     mysfit["mysfitId"] = item["MysfitId"]["S"]
-        mysfit["name"] = item["Name"]["S"]
-        mysfit["age"] = int(item["Age"]["N"])
-        mysfit["goodevil"] = item["GoodEvil"]["S"]
-        mysfit["lawchaos"] = item["LawChaos"]["S"]
-        mysfit["species"] = item["Species"]["S"]
-        mysfit["description"] = item["Description"]["S"]
-        mysfit["thumbImageUri"] = item["ThumbImageUri"]["S"]
-        mysfit["profileImageUri"] = item["ProfileImageUri"]["S"]
-	
-        mysfitList["mysfits"].append(mysfit)
+func getJSONStringFromItems(items []map[string]*dynamodb.AttributeValue) string {
+	return ""
 }
 
-result, err := svc.Scan(input)
+func getAllMysfitsDebug() string {
+	// Create a DynamoDB client using our default credentials and region.
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	// Create DynamoDB client
+	svc := dynamodb.New(sess)
+
+	// Retrieve all Mysfits from DynamoDB using the DynamoDB scan operation.
+	// Note: The scan API can be expensive in terms of latency when a DynamoDB
+	// table contains a high number of records and filters are applied to the
+	// operation that require a large amount of data to be scanned in the table
+	// before a response is returned by DynamoDB. For high-volume tables that
+	// receive many requests, it is common to store the result of frequent/common
+	// scan operations in an in-memory cache. DynamoDB Accelerator (DAX) or
+	// use of ElastiCache can provide these benefits. But, because out Mythical
+	// Mysfits API is low traffic and the table is very small, the scan operation
+	// will suit our needs for this workshop.
+
+	input := &dynamodb.ScanInput{
+		TableName: aws.String("MysfitsTable"),
+	}
+
+	result, err := svc.Scan(input)
+	if err != nil {
+		Info.Print("Got error scanning table:")
+		return ""
+	}
+
+	// loop through the returned mysfits and add their attributes to a new map
+	// that matches the JSON response structure expected by the frontend.
+	Init(os.Stdout)
+	Info.Print(result.Items)
+
+	jsonValue := getJSONStringFromItems(result.Items)
+
+	return jsonValue
 }
 
-func queryMysfits() {
-    // Create a DynamoDB client using our default credentials and region.
-    sess := session.Must(session.NewSessionWithOptions(session.Options{
-        SharedConfigState: session.SharedConfigEnable,
-    }))
+func queryMysfitsDebug(filter string, value string) string {
+	Init(os.Stdout)
 
-    // Create DynamoDB client
-    svc := dynamodb.New(sess)
+	// We only have two secondary indexes: GoodEvil(Index) and LawChaos(Index)
+	if filter != "GoodEvil" && filter != "LawChaos" {
+		Info.Print("We only allow quering for GoodEvil or LawChaos")
+		return ""
+	}
 
-    
-}
+	// Create a DynamoDB client using our default credentials and region.
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
 
+	// Create DynamoDB client
+	svc := dynamodb.New(sess)
 
+	// Use the DynamoDB scan API to retrieve mysfits from the table that are
+	// equal to the selected filter values.
+	input := &dynamodb.ScanInput{
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":a": {
+				S: aws.String(value),
+			},
+		},
+		FilterExpression: aws.String(filter + " = :a"),
+		TableName:        aws.String("MysfitsTable"),
+	}
 
-func main() {
+	result, err := svc.Scan(input)
+	if err != nil {
+		Info.Print("Got error getting item:")
+		return ""
+	}
 
+	jsonValue := getJSONStringFromItems(result.Items)
+	Info.Print(jsonValue)
 
+	return jsonValue
 }
--- module-3/app/service/mythicalMysfitsService.go
+++ module-3/app/service/mythicalMysfitsService.go
@@ -1,28 +1,47 @@
 package main
 
 import (
-    "net/http"
-    "io/ioutil"
+	"fmt"
+	"net/http"
 )
 
 // For http://localhost:8088
 func healthCheckResponse(w http.ResponseWriter, req *http.Request) {
-    w.Write([]byte("Nothing here, used for health check. Try /misfits instead."))
+	w.Write([]byte("Nothing here, used for health check. Try /misfits instead."))
 }
 
 // Show mysfits-response.json for http://localhost:8088/misfits
 func showMisfits(w http.ResponseWriter, r *http.Request) {
-    // Read Misfits data from file and show it
-    body, _ := ioutil.ReadFile("mysfits-response.json")
+	// Does request contain a 'filter' arg?, ala:
+	// http://localhost:8088/misfits?filter=value1&value=value2
+	fmt.Println("GET params were:", r.URL.Query())
 
-    // Let the web server know it's JSON
-    w.Header().Set("Content-Type", "application/json")
-    w.Write(body)
+	var items Items
+
+	// if only one expected
+	filter := r.URL.Query().Get("filter")
+	if filter != "" {
+		value := r.URL.Query().Get("value")
+		if value != "" {
+			// Call Getall
+			items = queryMysfits(filter, value)
+		}
+
+	} else {
+		items = getAllMysfits()
+	}
+
+	// Read Misfits data from file and show it
+	body := []byte(items)
+
+	// Let the web server know it's JSON
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(body)
 }
 
 func main() {
-    mux := http.NewServeMux()
-    mux.Handle("/", http.HandlerFunc(healthCheckResponse))
-    mux.Handle("/misfits", http.HandlerFunc(showMisfits))
-    http.ListenAndServe(":8088", mux)
+	mux := http.NewServeMux()
+	mux.Handle("/", http.HandlerFunc(healthCheckResponse))
+	mux.Handle("/misfits", http.HandlerFunc(showMisfits))
+	http.ListenAndServe(":8088", mux)
 }
