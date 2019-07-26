888de3a5ec4f92d0c062417824af615d5b0414ab
EC2 Default User <ec2-user@ip-172-31-39-155.ec2.internal>
2019-07-26 12:11:37 +0000

    Updates Module 2 to use the AWS CDK

---
+++ module-2/cdk-complete/bin/cdk.ts
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+import cdk = require("@aws-cdk/core");
+import 'source-map-support/register';
+import { StaticWebsiteStack } from "../lib/static-website-stack";
+import { NetworkStack } from "../lib/network-stack";
+import { EcrStack } from "../lib/ecr-stack";
+import { EcsStack } from "../lib/ecs-stack";
+import { CiCdStack } from "../lib/cicd-stack";
+
+const app = new cdk.App();
+new StaticWebsiteStack(app, "MythicalMysfits-Website");
+const networkStack = new NetworkStack(app, "MythicalMysfits-Network");
+const ecrStack = new EcrStack(app, "MythicalMysfits-ECR");
+const ecsStack = new EcsStack(app, "MythicalMysfits-ECS", {
+    vpc: networkStack.vpc,
+    ecrRepository: ecrStack.ecrRepository
+});
+new CiCdStack(app, "MythicalMysfits-CICD", {
+    ecrRepository: ecrStack.ecrRepository,
+    ecsService: ecsStack.ecsService.service
+});
+app.synth();
