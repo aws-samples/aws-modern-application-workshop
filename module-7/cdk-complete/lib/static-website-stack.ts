e6e3cd121768e3c41974f1b23de897aea563747a
Daniele Stroppa <stroppad@amazon.com>
2019-08-13 10:31:32 +0000

    Updates Module 7 to use the AWS CDK

---
+++ module-7/cdk-complete/lib/static-website-stack.ts
@@ -0,0 +1,70 @@
+import cdk = require("@aws-cdk/core");
+import cloudfront = require("@aws-cdk/aws-cloudfront");
+import iam = require("@aws-cdk/aws-iam");
+import s3 = require("@aws-cdk/aws-s3");
+import s3deploy = require("@aws-cdk/aws-s3-deployment");
+import path = require('path');
+
+export class StaticWebsiteStack extends cdk.Stack {
+
+  constructor(scope: cdk.Construct, id: string) {
+    super(scope, id);
+
+    // Obtain the content directory from CDK context
+    const webAppRoot = path.resolve(__dirname, '..', '..', 'web');
+
+    // Create a S3 bucket, with the given name and define the web index document as 'index.html'
+    const bucket = new s3.Bucket(this, "Bucket", {
+      websiteIndexDocument: "index.html"
+    });
+
+    // Obtain the cloudfront origin access identity so that the s3 bucket may be restricted to it.
+    const origin = new cloudfront.CfnCloudFrontOriginAccessIdentity(this, "BucketOrigin", {
+      cloudFrontOriginAccessIdentityConfig: {
+        comment: "mythical-mysfits"
+      }
+    });
+
+    // Restrict the S3 bucket via a bucket policy that only allows our CloudFront distribution
+    bucket.grantRead(new iam.CanonicalUserPrincipal(
+      origin.attrS3CanonicalUserId
+    ));
+
+    // A CDK helper that takes the defined source directory, compresses it, and uploads it to the destination s3 bucket.
+    new s3deploy.BucketDeployment(this, "DeployWebsite", {
+      source: s3deploy.Source.asset(webAppRoot),
+      destinationKeyPrefix: "web/",
+      destinationBucket: bucket,
+      retainOnDelete: false
+    });
+
+    // Definition for a new CloudFront web distribution, which enforces traffic over HTTPS
+    const cdn = new cloudfront.CloudFrontWebDistribution(this, "CloudFront", {
+      viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.ALLOW_ALL,
+      priceClass: cloudfront.PriceClass.PRICE_CLASS_ALL,
+      originConfigs: [
+        {
+          behaviors: [
+            {
+              isDefaultBehavior: true,
+              maxTtl: undefined,
+              allowedMethods:
+                cloudfront.CloudFrontAllowedMethods.GET_HEAD_OPTIONS
+            }
+          ],
+          originPath: `/web`,
+          s3OriginSource: {
+            s3BucketSource: bucket,
+            originAccessIdentityId: origin.attrS3CanonicalUserId
+          }
+        }
+      ]
+    });
+
+    // Create a CDK Output which details the URL for the CloudFront Distribtion URL.
+    new cdk.CfnOutput(this, "CloudFrontURL", {
+      description: "The CloudFront distribution URL",
+      value: "http://" + cdn.domainName
+    });
+  }
+}
